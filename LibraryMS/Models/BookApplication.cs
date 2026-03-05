b75f85bbb7d5141613c47ec2b2cc6568ae774876
Niponpal <niponpal52@gmail.com>
2026-03-05 16:58:54 +0600

    Fix all 🚀🚀🚑

---
+++ LibraryMS/Models/BookApplication.cs
@@ -0,0 +1,17 @@
+﻿namespace LibraryMS.Models
+{
+    public class BookApplication
+    {
+        public string StudentEmail { get; set; } = string.Empty;
+
+        public string StudentIdCardNo { get; set; } = string.Empty;
+        public long StudentId { get; set; }
+        public String Status { get; set; } = "Pending"; // Pending, Approved, Rejected
+        public Decimal FineAmount { get; set; } = 0;
+        public DateTime IssueDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public Book? Book { get; set; }
+        public int BookId { get; set; }
+    }
+}
