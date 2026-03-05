c9c1d8306500b08e636247ad4f32cc31a867f988
Niponpal <niponpal52@gmail.com>
2026-03-05 17:00:11 +0600

    Fix All Model 🚀🚀🚑

---
+++ LibraryMS/Models/BookCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryMS.Models
+{
+    public class BookCategory
+    {
+        public string CategoryName { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public ICollection<Book>? Books { get; set; }
+    }
+}
