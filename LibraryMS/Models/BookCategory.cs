c81665c21511b8c5992b028c8d782154a03ee35d
Niponpal <niponpal52@gmail.com>
2026-03-05 17:06:30 +0600

    Use BaseEntity Id 🚀🚀🚑

--- LibraryMS/Models/BookCategory.cs
+++ LibraryMS/Models/BookCategory.cs
@@ -4,7 +4,7 @@ namespace LibraryMS.Models;
 
 public class BookCategory: BaseEntity
 {
-    public Guid Id { get; set; }
+    //public Guid Id { get; set; }
     public string CategoryName { get; set; } = string.Empty;
     public string? Description { get; set; }
     public bool IsActive { get; set; } = true;
