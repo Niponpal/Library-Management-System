9f95ca0ee1336b83f5d4ff6fce231b9a379a52a6
Niponpal <niponpal52@gmail.com>
2026-03-06 20:18:48 +0600

    Fix AppliCationDbContext 🚀🚀🚑

---
+++ LibraryMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryMS.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Models.Book> Books { get; set; }
+        public DbSet<Models.BookCategory> BookCategories { get; set; }
+        public DbSet<Models.BookApplication> BookApplications { get; set; }
+    }
+}
