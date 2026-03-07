f0075b52ddd62ac5273d101dff697090dfe0cd3b
Niponpal <niponpal52@gmail.com>
2026-03-07 22:52:12 +0600

    Fix All BookApplicationRepository  🚀🚀🚑

--- LibraryMS/Repository/BookApplicationRepository.cs
+++ LibraryMS/Repository/BookApplicationRepository.cs
@@ -1,7 +1,77 @@
-﻿namespace LibraryMS.Repository
+﻿using LibraryMS.Data;
+using LibraryMS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryMS.Repository
 {
-    public class BookApplicationRepository
+    public class BookApplicationRepository: IBookApplicationRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public BookApplicationRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<BookApplication> AddBookApplicationAsync(BookApplication bookApplication, CancellationToken cancellationToken)
+        {
+            await _context.BookApplications.AddAsync(bookApplication, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return bookApplication;
+        }
+
+        public async Task<BookApplication> DeleteBookApplicationAsync(int id, CancellationToken cancellationToken)
+        {
+            var data = await _context.BookApplications.FindAsync(id);
+            if (data != null)
+            {
+                _context.BookApplications.Remove(data);
+                await _context.SaveChangesAsync(cancellationToken);
+                return data;
+            }
+            return null!;
+        }
+
+
+        public async Task<IEnumerable<BookApplication>> GetAllBookApplicationAsync(CancellationToken cancellationToken)
+        {
+            var data = await _context.BookApplications.ToListAsync(cancellationToken);
+            if (data != null)
+            {
+                return data;
+            }
+            return null;
+        }
+
+        public async Task<BookApplication> GetBookApplicationByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            var data = await _context.BookApplications.FindAsync(id);
+            if (data != null)
+            {
+                return data;
+            }
+            return null!;
+        }
+
+
+        public async Task<BookApplication> UpdateBookApplicationAsync(BookApplication bookApplication, CancellationToken cancellationToken)
+        {
+            var data = await _context.BookApplications.FindAsync(bookApplication.Id);
+            if (data != null)
+            {
+                data.StudentEmail = bookApplication.StudentEmail;
+                data.StudentId = bookApplication.StudentId;
+                data.StudentIdCardNo = bookApplication.StudentIdCardNo;
+                data.Status = bookApplication.Status;
+                data.FineAmount = bookApplication.FineAmount;
+                data.IssueDate = bookApplication.IssueDate;
+                data.ReturnDate = bookApplication.ReturnDate;
+                //data.BookId = bookApplication.BookId;
+                await _context.SaveChangesAsync(cancellationToken);
+                return data;
+            }
+            return null!;
+        }
 
     }
 }
