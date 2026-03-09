55939104216a236cd6161d49755941bee6ca3237
Niponpal <niponpal52@gmail.com>
2026-03-09 11:56:10 +0600

    Fix BookRepository 🚀🚀🚑

---
+++ LibraryMS/Repository/BookRepository.cs
@@ -0,0 +1,37 @@
+﻿using LibraryMS.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LibraryMS.Repository;
+
+public class BookRepository : IBookRepository
+{
+    public Task<Book> AddBookAsync(Book book, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Book> DeleteBooktAsync(int id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<SelectListItem> Dropdown()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Book>> GetAllBookAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Book> GetBookByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Book> UpdateBookAsync(Book book, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
