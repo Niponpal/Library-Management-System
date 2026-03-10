31f741f40d1633c600123feb3661fa064f19cc31
Niponpal <niponpal52@gmail.com>
2026-03-10 13:17:09 +0600

    Fix BookApplicationController  🚀🚀🚑

---
+++ LibraryMS/Controllers/BookApplicationController.cs
@@ -0,0 +1,95 @@
+﻿using LibraryMS.Models;
+using LibraryMS.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryMS.Controllers;
+
+public class BookApplicationController : Controller
+{
+    private readonly IBookApplicationRepository _bookApplicationRepository;
+    private readonly IBookRepository _bookRepository;
+
+
+    public BookApplicationController(IBookApplicationRepository bookApplicationRepository, IBookRepository bookRepository)
+    {
+        _bookApplicationRepository = bookApplicationRepository;
+        _bookRepository = bookRepository;
+
+    }
+
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var data = await _bookApplicationRepository.GetAllBookApplicationAsync(cancellationToken);
+        return View(data);
+    }
+    [HttpGet]
+    public async Task<IActionResult> CreateOrEdit(int id, CancellationToken cancellationToken)
+    {
+        ViewData["BookId"] = _bookRepository.Dropdown();
+        if (id == 0)
+        {
+
+            return View(new BookApplication());
+        }
+        else
+        {
+            var data = await _bookApplicationRepository.GetBookApplicationByIdAsync(id, cancellationToken);
+            if (data != null)
+            {
+
+                return View(data);
+            }
+            return NotFound();
+        }
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateOrEdit(BookApplication bookApplication, CancellationToken cancellationToken)
+    {
+        ViewData["BookId"] = _bookRepository.Dropdown();
+        if (bookApplication.Id == 0)
+        {
+            ViewData["BookId"] = _bookRepository.Dropdown();
+            //bookApplication.StudentId = _signInHelper.UserId ?? 1;
+            bookApplication.Status = "Pending";
+            await _bookApplicationRepository.AddBookApplicationAsync(bookApplication, cancellationToken);
+            return RedirectToAction("Index");
+        }
+        else
+        {
+
+            await _bookApplicationRepository.UpdateBookApplicationAsync(bookApplication, cancellationToken);
+            return RedirectToAction("Index");
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(int id, CancellationToken cancellationToken)
+    {
+        var data = await _bookApplicationRepository.GetBookApplicationByIdAsync(id, cancellationToken);
+        if (data != null)
+        {
+            return View(data);
+        }
+        return NotFound();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
+    {
+        await _bookApplicationRepository.DeleteBookApplicationAsync(id, cancellationToken);
+        return RedirectToAction("Index");
+    }
+    [HttpGet]
+    public async Task<IActionResult> Applybook(int id, CancellationToken cancellationToken)
+    {
+        BookApplication bookApplication = new BookApplication();
+        ViewData["BookId"] = _bookRepository.Dropdown();
+        if (id > 0)
+        {
+            var data = await _bookRepository.GetBookByIdAsync(id, cancellationToken);
+            //bookApplication.BookId = data.Id;
+            //bookApplication.StudentId = _signInHelper.UserId ?? 1;
+
+        }
+        return View(bookApplication);
+    }
+}
