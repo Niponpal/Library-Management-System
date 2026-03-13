a8f511b2b3901848d1fe7897310eff7cd6b2090e
Niponpal <niponpal52@gmail.com>
2026-03-13 15:33:19 +0600

    Fix AccountController 🚀🚀🚑

---
+++ LibraryMS/Controllers/AccountController.cs
@@ -0,0 +1,97 @@
+﻿using LibraryMS.Models.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using static LibraryMS.Auth_IdentityModel.IdentityModel;
+
+namespace LibraryMS.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly SignInManager<User> _signInManager;
+    private readonly UserManager<User> _userManager;
+    private readonly IAuthService _authService;
+    public AccountController(
+      SignInManager<User> signInManager,
+      UserManager<User> userManager,
+      IAuthService authService)
+    {
+        _signInManager = signInManager;
+        _userManager = userManager;
+        _authService = authService;
+    }
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult Register()
+    {
+        return View(new RegisterViewModel());
+    }
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(RegisterViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        var result = await _authService.Register(model);
+
+        if (!result.Success)
+        {
+            result.Errors.ForEach(e => ModelState.AddModelError("", e));
+            return View(model);
+        }
+
+        var user = await _signInManager.UserManager
+            .FindByIdAsync(result.UserId.ToString());
+
+        if (user != null)
+        {
+            await _signInManager.SignInAsync(user, false);
+        }
+        return RedirectToAction("Index", "Dashboard");
+
+
+    }
+
+
+
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return View(model);
+
+        var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+        if (result.Succeeded)
+            return LocalRedirect("/Dashboard/Index");
+        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+        return View(model);
+    }
+    [HttpPost]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
+
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult AccessDenied(string returnUrl = null)
+    {
+        ViewData["ReturnUrl"] = returnUrl;
+        return View();
+    }
+
+}
